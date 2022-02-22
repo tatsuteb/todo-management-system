5ea2387fc7ab68d1b8306578f4237cde7f962f2d
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2022-02-23 01:09:38 +0900

    開始日、期日を送信、表示するように修正

--- src/WebClient/Models/Todos/TodoDetailsViewModel.cs
+++ src/WebClient/Models/Todos/TodoDetailsViewModel.cs
@@ -4,6 +4,8 @@
     {
         public string Id { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public DateTime? BeginDateTime { get; set; }
+        public DateTime? DueDateTime { get; set; }
         public string? Description { get; set; }
         public int Status { get; set; }
         public DateTime CreatedDateTime { get; set; }
