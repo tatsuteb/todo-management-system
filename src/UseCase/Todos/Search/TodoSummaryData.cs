07ead14e773c03cedce9018064f8398396e14922
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2022-02-23 01:07:14 +0900

    Todo検索のテストと実装を作成

--- src/UseCase/Todos/Search/TodoSummaryData.cs
+++ src/UseCase/Todos/Search/TodoSummaryData.cs
@@ -4,19 +4,25 @@
     {
         public string Id { get; }
         public string Title { get; }
+        public DateTime? BeginDateTime { get; }
+        public DateTime? DueDateTime { get; }
         public DateTime CreatedDateTime { get; }
         public DateTime UpdatedDateTime { get; }
         public string StatusString { get; }
 
         public TodoSummaryData(
             string id, 
-            string title, 
+            string title,
+            DateTime? beginDateTime,
+            DateTime? dueDateTime,
             DateTime createdDateTime, 
             DateTime updatedDateTime, 
             string statusString)
         {
             Id = id;
             Title = title;
+            BeginDateTime = beginDateTime;
+            DueDateTime = dueDateTime;
             CreatedDateTime = createdDateTime;
             UpdatedDateTime = updatedDateTime;
             StatusString = statusString;
