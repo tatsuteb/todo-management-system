a20150a2586e80048f4800eb248fe73d4ef98c20
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2022-02-22 23:51:11 +0900

    ユースケース、ドメイン層で投げたエラー詳細がプレゼンテーション層に届かないバグを修正

--- src/UseCase/Todos/Edit/TodoEditUseCase.cs
+++ src/UseCase/Todos/Edit/TodoEditUseCase.cs
@@ -1,6 +1,5 @@
-﻿using System.Net;
+﻿using Domain.Models.Todos;
 using System.Transactions;
-using Domain.Models.Todos;
 using UseCase.Shared;
 
 namespace UseCase.Todos.Edit
@@ -18,37 +17,27 @@ namespace UseCase.Todos.Edit
         {
             using var ts = new TransactionScope();
 
-            try
+            var todo = await _todoRepository.FindAsync(new TodoId(command.Id));
+
+            if (todo is null)
             {
-                var todo = await _todoRepository.FindAsync(new TodoId(command.Id));
-
-                if (todo is null)
-                {
-                    throw new UseCaseException("指定されたTODOが見つかりません。");
-                }
-
-                if (todo.OwnerId.Value != command.UserSession.Id)
-                {
-                    throw new UseCaseException("指定されたTODOを編集する権限がありません。");
-                }
-
-                todo.Edit(
-                    title: new TodoTitle(command.Title),
-                    description: !string.IsNullOrWhiteSpace(command.Description)
-                        ? new TodoDescription(command.Description)
-                        : null);
-
-                await _todoRepository.SaveAsync(todo);
-
-                ts.Complete();
+                throw new UseCaseException("指定されたTODOが見つかりません。");
             }
-            catch (Exception e)
+
+            if (todo.OwnerId.Value != command.UserSession.Id)
             {
-                ts.Dispose();
-
-                Console.WriteLine(e);
-                throw new UseCaseException("TODOの編集に失敗しました。", (int)HttpStatusCode.InternalServerError);
+                throw new UseCaseException("指定されたTODOを編集する権限がありません。");
             }
+
+            todo.Edit(
+                title: new TodoTitle(command.Title),
+                description: !string.IsNullOrWhiteSpace(command.Description)
+                    ? new TodoDescription(command.Description)
+                    : null);
+
+            await _todoRepository.SaveAsync(todo);
+
+            ts.Complete();
         }
     }
 }
